aeebb6eb9297d02f406e3c49ce74115ffba25483
Marcin Klisz <marcin.klisz@icloud.com>
2022-05-17 11:27:52 +0200

    stworzenie apki lab4

---
+++ Lab4/Lab4/Lab4App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab4App.swift
+//  Lab4
+//
+//  Created by Marcin Klisz on 17/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Lab4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
