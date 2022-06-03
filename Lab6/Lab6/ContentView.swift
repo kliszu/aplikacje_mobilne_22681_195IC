d08e10563db55bba0ed1fd83638f409d49630e9a
Marcin Klisz <marcin.klisz@icloud.com>
2022-06-03 18:46:28 +0200

    update README.md

---
+++ Lab6/Lab6/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lab6
+//
+//  Created by Marcin Klisz on 03/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lab6/Lab6/Lab6App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab6App.swift
+//  Lab6
+//
+//  Created by Marcin Klisz on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Lab6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
